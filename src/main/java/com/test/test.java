a4c5e5b8cb9500ad8aa6a0faeb755e5a146b18e3
w952982340 <wwxyy0525@126.com>
2018-09-03 08:48:20 +0800

    test

---
+++ src/main/java/com/test/test.java
@@ -0,0 +1,11 @@
+package com.test;
+
+/**
+ * Created by WY on 2018-09-03.
+ */
+public class test {
+
+    public static void main (String[] args){
+        System.out.println("15464545");
+    }
+}
